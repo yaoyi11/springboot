4aa8518642e925fdc0fab8d5253ccb7e274f786e
yaoyi11 <767248521@qq.com>
2020-10-13 21:04:18 +0800

    add learn branch

---
+++ learn/src/main/java/org/learn/example/HelloController.java
@@ -0,0 +1,12 @@
+package org.learn.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hhhh,yaoyi";
+    }
+}
